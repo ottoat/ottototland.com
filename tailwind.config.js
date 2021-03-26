923c2de7f9fff164b0b97dae592cbab4b9a5de6e
Rune Warhuus <rune@dinkdonk.no>
2021-03-26 23:47:34 +0100

    Temp site

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+	  sans: ['Barlow Condensed', 'sans-serif']
+	},
+    container: {
+      center: true,
+      padding: '2rem'
+	},
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
