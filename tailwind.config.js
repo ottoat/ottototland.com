9c97fe85fb1bc38ce96c7c080fa8912f33c7d4f1
Rune Warhuus <rune@dinkdonk.no>
2021-03-29 00:19:23 +0200

    Piano day!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,51 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-	  sans: ['Barlow Condensed', 'sans-serif']
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		colors: {
+			yellow: '#C3A741',
+			white: '#F3EDD7',
+			green: '#4C544B',
+			blue: '#304E5E',
+			beige: '#B1AA93',
+			sienna: {
+				DEFAULT: '#5E3630',
+				dark: '#180E0C'
+			}
+		},
+		fontFamily: {
+			sans: ['Barlow Condensed', 'sans-serif']
+		},
+		letterSpacing: {
+			tightest: '-.075em',
+			tighter: '-.05em',
+			tight: '-.025em',
+			normal: '0',
+			wide: '.025em',
+			wider: '.05em',
+			widest: '.1em',
+			widest: '.35em',
+		},
+		container: {
+			center: true,
+			padding: '2rem'
+		},
+		extend: {
+			lineHeight: {
+				'extra-tight': 0.75
+			},
+			translate: {
+				'3/100': '3%',
+				'-3/100': '-3%'
+			},
+			rotate: {
+				'-5': '-5deg',
+				'5': '5deg'
+			}
+		},
 	},
-    container: {
-      center: true,
-      padding: '2rem'
+	variants: {
+		extend: {},
 	},
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	plugins: [],
 }
